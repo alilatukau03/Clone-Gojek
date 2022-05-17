245772eff8beedacb17b79cda1437b68aeb8e675
alilatukau03 <alilatukau07@gmail.com>
2022-05-17 09:24:14 +0700

    status bar done

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
 import React, {Component} from 'react';
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, TextInput} from 'react-native';
 
 export default class App extends Component {
   render() {
     return (
       <View style={{flex: 1}}>
-        <View style={{flex: 1, backgroundColor: '#e3e1dc'}}>
-          <Text>Clone Gojek</Text>
+        <View style={{flex: 1, backgroundColor: 'white', paddingTop: 15}}>
+          <View style={{marginHorizontal: 17, flexDirection: 'row'}}>
+            <View style={{position: 'relative', flex: 1}}>
+              <TextInput
+                placeholder="what do you want to eat?"
+                style={{
+                  borderWidth: 1,
+                  borderColor: '#e8e8e8',
+                  borderRadius: 25,
+                  height: 40,
+                  fontSize: 13,
+                  paddingLeft: 45,
+                  paddingRight: 20,
+                  backgroundColor: 'white',
+                  marginRight: 18,
+                }}
+              />
+              <Image
+                source={require('./Icon/search.png')}
+                style={{position: 'absolute', top: 6, left: 12}}
+              />
+            </View>
+            <View
+              style={{
+                width: 35,
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}>
+              <Image source={require('./Icon/promo.png')} />
+            </View>
+          </View>
         </View>
         <View
           style={{
