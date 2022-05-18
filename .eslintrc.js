437cc70cb59d1f9a51dc9db572f566789080fdde
alilatukau03 <alilatukau07@gmail.com>
2022-05-18 09:56:12 +0700

    hardcore done

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,6 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  // root: true,
+  // extends: '@react-native-community',
+  requireConfigFile: false,
+  parser: '@babel/eslint-parser',
 };
